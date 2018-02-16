cea6097d636763eb8ebbaf9ddbba3cd51d644ee5
Alicia Baker <aliciabaker314@gmail.com>
2018-02-16 10:36:30 -0700

    3rd try

--- Palindrome.rb
+++ Palindrome.rb
@@ -4,18 +4,20 @@
 # Add code to that ruby script to ask for user input and check to see if that user input is a palindrome or not.
 # DO NOT USE any STRING helper methods like .reverse this is a coding challenge to see how you would write all of the code to do this!
 # Have your script go in a loop or a recursive method call so the user can keep checking palindromes.
+require 'colorize'
+
 
 def rev_string
-  puts "please enter a string and I will check to see if its a pallindrome"
+  puts "Enter a word, and I will check to see if its a pallindrome!".magenta
   string = gets.strip.downcase
   splits = string.split("")
-  puts "thank you. checking............"
+  puts "Thanks, I'll check now............".green
   rev = []
   string.size.times {rev << splits.pop}
   rev.join
   if rev.join == string
-    puts "NICE!  totally a pallindrome"
-  else puts " hmmm looks like its not a pallindrome"
+    puts "NICE! The word you chose IS a palindrome!".yellow
+  else puts "Bummer, that word is NOT a palindrome.".red
   end
 
 end
